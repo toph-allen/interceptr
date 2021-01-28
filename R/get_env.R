d64ff4a661e8fe140d557e30e9fb705eebf1287e
Toph Allen <toph.allen@gmail.com>
2021-01-28 14:36:45 -0500

    Refactoring and expanding

--- R/get_env.R
+++ R/get_env.R
@@ -1,13 +1,27 @@
-get_env <- function(vars) {
+get_env <- function(vars = NULL, context = "rmd") {
   found_vars <- na.omit(Sys.getenv(vars, unset = NA))
   missing_vars <- setdiff(vars, names(found_vars))
 
-  if (length(missing_vars) > 0) {
-    print("The following environment variables could not be found:")
-    print(missing_vars)
-    knitr::knit_exit()
-    return()
+  if (length(found_vars) == length(vars)) {
+    return(as.list(found_vars))
   } else {
-    return(found_vars)
+    if (context == "rmd") {
+      rmd_missing_vars(missing_vars)
+    }
   }
+}
+
+rmd_missing_vars <- function(missing_vars) {
+  message <- paste(
+    "The following environment variables could not be found:",
+    missing_vars,
+    ifelse(
+      is_connect(),
+      "Please enter these variable to the right. ->",
+      "Please ensure these variables are available"),
+    sep = "\n",
+    collapse = ", "
+  )
+  cat(message)
+  knitr::knit_exit()
 }
